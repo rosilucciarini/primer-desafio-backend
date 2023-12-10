d4ded39e2d8c24fc6ae32b6037dbf9a46e133d6c
rosa lina lucciarini <rosilucciarini@gmail.com>
2023-12-09 23:54:43 -0300

    segundo desafio y creacion de archivo json

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
+const fs = require("fs").promises;
+
+
 class ProductManager {
 
     static ultimoId = 0;
 
-    constructor(){
+    constructor(path){
         this.products = [];
+        this.path = path;
     }
 
-    addProduct(title, description, price, thumbnail, code, stock) {
+    async addProduct(nuevoOjeto) {
+
+        let {title, description, price, thumbnail, code, stock} = nuevoOjeto;
+
 
         if(!title || !description || !price || !thumbnail || !code || !stock) {
             console.log("Todos los campos son obligatorios");
@@ -31,39 +38,167 @@ class ProductManager {
        
         this.products.push(newProduct);
         
+
+        await this.guardarArchivo(this.products);
+        
     }
 
     getProducts() {
         console.log(this.products);
     }
 
-    getProductById(id) {
-        const product = this.products.find(item => item.id === id);
+    async getProductById(id) {
+        try {
+            const arrayProductos = await this.leerArchivo();
+            const buscado = arrayProductos.find(item => item.id === id);
 
-        if(!product) {
-            console.log("Producto no encontrado")
-        }else {
-            console.log("Producto encontrado:", product);
+            if(!buscado) {
+                console.log("Producto no encontrado");
+            } else{
+                console.log("Producto encontrado!");
+                return buscado;
+            }
+
+        } catch (error) {
+            console.log("Error al leer el archivo", error)
+            
         }
-        return product;
+    
     }
+
+
+
+    async leerArchivo() {
+        try{
+             const respuesta = await fs.readFile(this.path, "utf-8");
+             const arrayProductos = JSON.parse(respuesta);
+             return arrayProductos;
+
+        }catch (error) {
+            console.log("Error al leer archivo", error)
+
+        }
+    }
+    async guardarArchivo(arrayProductos) {
+        try{
+            await fs.writeFile(this.path, JSON.stringify(arrayProductos, null, 2))
+
+        }catch(error) {
+            console.log("Error al guardar el archivo", error)
+
+        }
+    }
+
+    async updateProduct(id, productoActualizado) {
+        try {
+            const arrayProductos = await this.leerArchivo();
+
+            const index = arrayProductos.findIndex(item => item.id === id);
+
+            if(index !== -1){
+                arrayProductos.splice(index, 1, productoActualizado);
+                await this.guardarArchivo(arrayProductos);
+            }else {
+                console.log("No se encontro el archivo");
+            }
+        } catch (error) {
+            console.log("Error al actualizar el producto", error);
+            
+        }
+    }
+
+    async deleteProduct(id) {
+        try {
+            const arrayProductos = await this.leerArchivo();
+
+            const index = arrayProductos.findIndex(item => item.id === id);
+
+            if(index !== -1){
+               arrayProductos.splice(index, 1);
+                await this.guardarArchivo(arrayProductos);
+                console.log("Producto eliminado:",deleteProduct)
+            }else {
+                console.log("Producto no encontrado");
+            }
+        } catch (error) {
+            console.log("Error al actualizar el producto", error);
+            
+        }
+    }
+
+    
 
 }
 
+//TESTIN
 
+//Instancia ProductManager
 
-const manager = new ProductManager();
+const manager = new ProductManager("./productos.json");
 
-manager.getProducts();
-
-manager.addProduct("Remera", "Remera deportiva", 15000, "sin imagen", "abcd123", 20);
-manager.addProduct("Short", "Short deportivo", 25000, "sin imagen", "abcd124", 40);
-manager.addProduct("Buzo", "Buzo deportivo color gris", 35000, "sin imagen", "abcd125", 15);
+//GetProducts que devuelve array vacio
 
 manager.getProducts();
 
-manager.getProductById(2);
-manager.getProductById(10);
+//Creacion de AddProduct
+
+const remera ={
+    title: "Remera",
+    description: "remera magas cortas",
+    price: 20000,
+    thumbnail: "sin imagen",
+    code: "abc123",
+    stock: 20
+}
+manager.addProduct(remera);
+
+//Objeto agregado con ID generado sin repetirse
+
+const pantalon ={
+    title: "Pantalon",
+    description: "pantalon adidas",
+    price: 30000,
+    thumbnail: "sin imagen",
+    code: "abc124",
+    stock: 20
+}
+manager.addProduct(pantalon);
+
+manager.getProducts();
+
+//Metodo getProductById que devuelve el producto y en caso de no existir devuelve un error.
+
+async function testeamosBusquedaId() {
+    const buscado = await manager.getProductById(2);
+    console.log(buscado);
+}
+
+testeamosBusquedaId();
+
+//Metodo updateProduct intenta cambiar el campo de un producto, se actualiza sin eliminar el Id.
+
+const short ={
+    id:1,
+    title: "Short",
+    description: "short corto hombre",
+    price: 30000,
+    thumbnail: "sin imagen",
+    code: "abc123",
+    stock: 20
+};
+async function testeamosActualizar(){
+    await manager.updateProduct(1, short);
+}
+testeamosActualizar();
+
+//Metodo deleteProduct elimina el producto que tenga el Id que se pasa por parametro.
+//Esta funcion funciona si no se actualiza el producto antes.
+
+
+async function testeoDelete(){
+    await manager.deleteProduct(1);
+}
+testeoDelete();
 
 
 
